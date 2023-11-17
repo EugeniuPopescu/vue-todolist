cf5e2a16124dd86ef35921340cd3b27ae7083fdb
Eugen <epopescuu@yahoo.it>
2023-11-17 17:50:34 +0100

    done: false

--- main.js
+++ main.js
@@ -3,11 +3,38 @@ const { createApp } = Vue;
 const opzioni = {
     data: function () {
         return {
-            message: 'ToDO',
-            
+            message: 'ToDO list',
+            todoList: [
+                {
+                    text: 'andare in palestra',
+                    done: false
+                },
+                {
+                    text: 'fare il ToDO',
+                    done: false
+                },
+                {
+                    text: 'studiare VUE',
+                    done: false
+                },
+                {
+                    text: 'preparare i pasti',
+                    done: false
+                },
+                {
+                    text: 'fare la lavatrice',
+                    done: false
+                },
+                {
+                    text: 'comprare i giochi per ps5',
+                    done: false
+                }
+            ]
         }
     },
-   
+   methods: {
+        
+   }
 };
 
 createApp(opzioni).mount('#app')
