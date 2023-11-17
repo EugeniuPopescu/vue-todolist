d1b4eb5cab5119d33cdcef08193142ee02fd1984
Eugen <epopescuu@yahoo.it>
2023-11-17 16:11:42 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const { createApp } = Vue;
+
+const opzioni = {
+    data: function () {
+        return {
+            message: 'ToDO',
+            
+        }
+    },
+   
+};
+
+createApp(opzioni).mount('#app')
