df67c6bfeddc1365fc7f339aee2e0b67a8b492f0
Eugen <epopescuu@yahoo.it>
2023-11-20 16:45:59 +0100

    feat: add coments and function toggle

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const opzioni = {
         return {
             message: 'ToDO list',
             newTodo: '',
+            inputError: false,
             todoList: [
                 {
                     text: 'andare in palestra',
@@ -36,17 +37,42 @@ const opzioni = {
     },
     methods: {
         deleteBtn(index) {
+            // l'indice lo passo al metodo (così capisco quale item lavoro)
+            // splice è un metodo, ha bisogno di 2 proprietà:
+            // 1) indice che mi dice suq aule item lavoro
+            // 2) quanti elementi voglio elimninare
             this.todoList.splice(index, 1);
         },
-        addBtn()  {
+        addItem()  {
+            // oggetto che mi creo uguale all'oggetto task
             let todoNew = {
                 text: this.newTodo,
                 done: false
             };
             
-            this.todoList.push(todoNew);
-            this.newTodo = '';
-        }     
+            // add controller to todoList text field
+            if (this.newTodo == '' || this.newTodo.length < 3) {
+                // imposto true così mi da l'errore
+                this.inputError = true;
+                // alert('Inserisci testo');
+            } else {
+                // .push() aggiunge alla coda
+                // unshift aggiunge all'inizio 
+                this.todoList.unshift(todoNew);
+                this.newTodo = '';
+
+                // così resetto lo stato di errore, posso procedere
+                this.inputError = false;
+            }
+        },
+        // metodo di toggle per il done
+        checkItem(index) {
+            // controllo se esiste l'oggetto
+            if (this.todoList[index]) {
+                // dall'array vai in posizione index e cambia la proprietà con il contrario di quello che c'è già
+                this.todoList[index].done = !this.todoList[index].done;
+            }
+        }
     }
 };
 
